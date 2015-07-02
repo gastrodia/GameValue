f47a84d860c1db7599ccb6d92728d2f9ab298320
xuyang <xuyang@wozlla.com>
2015-07-02 16:16:53 +0800

    table-finish

--- src/components/data-view/DataView.ts
+++ src/components/data-view/DataView.ts
@@ -11,6 +11,8 @@ socket.on('outlet.update', function (data) {
   console.log(data);
 });
 
+var selected:JQuery;
+
 
 @Component({
   selector: 'data-view'
@@ -25,9 +27,122 @@ socket.on('outlet.update', function (data) {
 })
 
 class DataView{
+  ngElement;
+  $elem:JQuery;
+
+  titleArray:Array<any>;
+  data:Array<Array<any>>;
 
   constructor(viewContainer: core.ViewContainerRef) {
     //this.parent = helper.getParentFromViewContainer(viewContainer);
+    this.ngElement =viewContainer.element;
+    var domElement:HTMLElement = (<any>this.ngElement).domElement;
+    this.$elem = $(domElement);
+
+    this.titleArray = ['name','color','scene','initShow'];
+
+    this.data = [
+      ['小明','red','scene_01','12'],
+      ['小红','gray','scene_02','13']
+    ]
+
+    this.drawTable();
+  }
+
+
+
+  editable_cell(warp:string,val:any):JQuery{
+    var cell = $(warp);
+    cell.text(val);
+    cell.click(()=>{
+      if(selected){
+        selected.css('background','');
+      }
+      cell.css('background','black');
+      cell.focus()
+      selected = cell;
+    });
+    cell.blur(()=>{
+        cell.css('background','');
+    });
+    cell.attr('contenteditable','');
+    return cell;
+  }
+
+  editable_th(i):JQuery{
+    var th = this.editable_cell('<th>',this.titleArray[i]);
+    return th;
+  }
+
+  editable_td(i,j):JQuery{
+    var td = this.editable_cell('<td>',this.data[i][j]);
+    td.keyup(()=>{
+      console.log(td.text());
+    })
+    return td;
+  }
+
+  add_column(){
+    this.titleArray.push('');
+    for(var i=0;i<this.data.length;i++){
+      this.data[i].push('');
+    }
+    this.redrawTable();
+  }
+
+  add_row(){
+    var newRow = [];
+    for(var i=0; i < this.titleArray.length;i++){
+      newRow.push('');
+    }
+    this.data.push(newRow);
+    this.redrawTable();
+  }
+
+  redrawTable(){
+    var tableContent:JQuery = this.$elem.find('.content');
+    tableContent.html('');
+    this.drawTable();
+  }
+
+  drawTable(){
+    var tableContent:JQuery = this.$elem.find('.content');
+    var table = $('<table>');
+    var title = $('<tr>');
+
+
+    for(var i=0;i<this.titleArray.length;i++){
+      var th = this.editable_th(i);
+      title.append(th);
+    }
+    var addColumnBtn = $('<th class="add-col-btn"><img src="public/images/add-btn-icon.png" alt=""></th>');
+    title.append(addColumnBtn);
+    table.append(title);
+
+    addColumnBtn.click(()=>{
+      this.add_column();
+    });
+
+
+    for(var i=0;i<this.data.length;i++){
+      var row = $('<tr>');
+      for(var j=0;j<this.data[i].length;j++){
+        var td = this.editable_td(i,j)
+        row.append(td);
+      }
+      table.append(row);
+    }
+    var lastRow = $('<tr>');
+    var addRowBtn = $('<td class="add-row-btn"><img src="public/images/add-btn-icon.png" alt=""></td>');
+    lastRow.append(addRowBtn);
+    table.append(lastRow);
+
+    addRowBtn.click(()=>{
+      this.add_row();
+    })
+
+    tableContent.append(table);
+
   }
 
 }
